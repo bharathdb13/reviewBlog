b8c6fcbbbfd6d15d3bf4e42cc639aedb1a7e377d
bharathdb13 <bharathbyregowda1@gmail.com>
2025-04-10 16:56:18 +0530

    updates about cors blocking

--- my-blog/config/middlewares.ts
+++ my-blog/config/middlewares.ts
@@ -1,7 +1,16 @@
 module.exports = [
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173', 'https://your-frontend-domain.com'], // add frontend URLs
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      headers: '*',
+      credentials: true,
+    },
+  },
   'strapi::security',
-  'strapi::cors',
+  // 'strapi::cors',
   'strapi::query',
   'strapi::body',
   'strapi::session',
